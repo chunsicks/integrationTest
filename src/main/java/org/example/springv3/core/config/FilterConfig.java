058bbecdeff0425a638ee0ed20819bcccef12459
sudal1 <ckddbsdbs123@naver.com>
2024-10-18 14:53:21 +0900

    필터추가

--- src/main/java/org/example/springv3/core/config/FilterConfig.java
+++ src/main/java/org/example/springv3/core/config/FilterConfig.java
@@ -1,6 +1,7 @@
 package org.example.springv3.core.config;
 
 import jakarta.servlet.FilterRegistration;
+import org.example.springv3.core.filter.CorsFilter;
 import org.example.springv3.core.filter.JwtAuthorizationFilter;
 import org.example.springv3.user.User;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -14,6 +15,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FilterConfig {
 
+    @Bean
+    public FilterRegistrationBean<?> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter());
+        //언제 동작하게 할것인지 정해야 한다
+        bean.addUrlPatterns("/*");
+        bean.setOrder(0);
+        return bean;
+    }
+    //기존거
     @Bean
     public FilterRegistrationBean<?> jwtAuthorizationFilter(){
         FilterRegistrationBean<JwtAuthorizationFilter> bean
